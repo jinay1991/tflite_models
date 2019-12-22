0a4cc3390d180dc267aad3e78d90589e259a624c
Jinay Patel <jinay1991@gmail.com>
2019-12-22 13:34:19 +0100

    Add Unit Tests

---
+++ lib/test/src/interpreter_tests.cpp
@@ -0,0 +1,19 @@
+///
+/// @file
+///
+#include <gtest/gtest.h>
+
+#include "tflite_models/interpreter.h"
+
+namespace tflite_models
+{
+namespace
+{
+class InterpreterFixture : public ::testing::Test
+{
+};
+
+TEST_F(InterpreterFixture, Null) {}
+
+}  // namespace
+}  // namespace tflite_models
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include <experimental/filesystem>
 #include <iostream>
 
-#include "tensorflow/lite/interpreter.h"
-
 int main(int argc, char** argv)
 {
     if (std::experimental::filesystem::exists("external/mobilenet_v2_1.0_224_quant/mobilenet_v2_1.0_224_quant.tflite"))
